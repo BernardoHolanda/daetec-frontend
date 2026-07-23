3b63e4d002c336e5bf8a4d903aea57629434cc37
Bernardo Holanda <bernardoholada@gmail.com>
2026-07-23 19:21:30 +0000

    feat: adiciona cliente axios com interceptor de JWT

---
+++ src/services/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+})
+
+// anexa o token JWT em toda requisição, se existir
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('daetec_token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default api
